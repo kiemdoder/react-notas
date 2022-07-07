4230c35b8bad1f58066b8a39360d46682d20e5a7
Roelf <roelf.de.kock@spandigital.com>
2022-07-07 08:39:25 +0200

    added list scrolling demo

--- src/pages/lists/Lists.tsx
+++ src/pages/lists/Lists.tsx
@@ -1,8 +1,10 @@
+import "./Lists.scss";
+
 import { SimpleListItem } from "./SimpleListItem";
 import { DetailedListItem } from "./DetailedListItem";
 import { RegularList } from "../../components/containers/List/RegularList";
 
-const listData = Array.from({ length: 10 }).map((_, i) => ({
+const listData = Array.from({ length: 50 }).map((_, i) => ({
   name: "Name" + i,
   number: i,
   enabled: i % 3 === 0,
@@ -10,19 +12,27 @@ const listData = Array.from({ length: 10 }).map((_, i) => ({
 
 export const ListsPage = () => {
   return (
-    <>
-      <RegularList
-        items={listData}
-        idFn={(it) => it.number}
-        itemSelected={(it) => console.log("item selected", it)}
-        resourceName={"listItem"}
-        itemComponent={SimpleListItem}
-      />
-      <RegularList
-        items={listData}
-        resourceName={"listItem"}
-        itemComponent={DetailedListItem}
-      />
-    </>
+    <div className="page">
+      <h3>List 1</h3>
+      <div className="content-wrapper">
+        <div className="overflow-container">
+          <RegularList
+            items={listData}
+            idFn={(it) => it.number}
+            itemSelected={(it) => console.log("item selected", it)}
+            resourceName={"listItem"}
+            itemComponent={SimpleListItem}
+          />
+        </div>
+      </div>
+      <h3>List 2</h3>
+      <div className="list-container">
+        <RegularList
+          items={listData}
+          resourceName={"listItem"}
+          itemComponent={DetailedListItem}
+        />
+      </div>
+    </div>
   );
 };
